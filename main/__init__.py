6f680f18c38d8ba82b6b0c0081e31d68f043ea77
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2024-10-27 08:23:30 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = "24894984" #config("API_ID", default=None, cast=int)
+API_HASH = "4956e23833905463efb588eb806f9804" #config("API_HASH", default=None)
+BOT_TOKEN = "7601293855:AAG0J1lG04wfEnK9J1GnImfOWN7X_2wyaS4" #config("BOT_TOKEN", default=None)
+SESSION = "BQDNvmIAc2pCfy2TNwMsVQLUU3GjqNuJlYYq_mjlI6P06s6bwaXavLJwzSpREBdv2ckEr8oy-ZAk5FDVLW9EWqpee2b98CWmUDu2xq8kMveTugmPun9Y19IFNTc7d78Tc5NzAtjusgPETXDuUyxP7TIF25HDD6MyzfoRMJMNQNa7jtdMKehOSYdxged-mdYluK7PecGD62AL9NIuaat5H91UAdL8qyNqYjNlkRzi-C0BKBGIIn_MeHiQRK12QKe6Bv8E4DFHxi5wKS2DGYpr1foaVxgHmibV7ly1rZ2e-_6y-5A7COhAUTQvNF0sCCfsei3mS7T1kVgSy2Kun4QfE5xgRAJDQwAAAAA1y9g-AA" #config("SESSION", default=None)
+FORCESUB = "Targetallcourse" #config("FORCESUB", default=None)
+AUTH = "902551614" #config("AUTH", default=None)
+SUDO_USERS = []
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
